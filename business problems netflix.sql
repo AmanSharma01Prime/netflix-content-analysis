23b14970274b05695ed258be1b1549fab5cedf6e
Aman Sharma <amansharma01prime@gmail.com>
2025-09-28 00:56:12 +0530

    Add files via upload

---
+++ business problems netflix.sql
@@ -0,0 +1,23 @@
+--15 Business Problems Netflix
+
+01. Count the Number of Movies vs TV Shows in the dataset.
+02. Trend of documentary releases per year
+03. Count the number of titles under each genre.
+04. Find the most common rating for Movies and TV Shows.
+05. Identify top 10 countries producing the highest number of Netflix titles
+
+06. Find top 10 directors with the highest number of titles.
+07. Find the top 10 actors who have appeared in the highest number of movies produced in India.
+08. Identify the longest duration movie and the TV show with the most seasons.
+09. Count titles containing keywords (‘love’, ‘action’, ‘violence’)
+10. Find each year and the average numbers of content release in India on netflix. 
+    return top 5 year with highest avg content release!
+
+11. Distribution of release duration for movies and seasons for TV shows
+12. A. Count titles released per year (2015–2021) 
+    B. Compare growth of Movies vs TV Shows over time
+13. Root cause analysis: Investigate TV show surge post-2015
+14. Categorize content based on keywords 'kill' and 'violence' in description. Label content as 'Bad' and all other 
+as 'Good'. Count how many items fall into each category.
+15. Top 5 genres by country – regional preference analysis
+
